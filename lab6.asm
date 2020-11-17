b8cd82efb3cd1dbd956565def31d930bb75cf7dd
AdamKorzun <45329904+AdamKorzun@users.noreply.github.com>
2020-11-17 21:52:35 +0300

    lab6

--- lab6.asm
+++ lab6.asm
@@ -1,33 +1,50 @@
 .model small
 .stack 1000h
+.186
 .data 
-    old_9h dw 0,0
+    old_9h dd 0
+
 
 .code 
-kb_handler proc
-    push DX
-    push AX
-    mov DL, 'p'
-    mov AH, 02h     
-    int 21h
-    
-    pop AX
-    pop DX
-    
-    ret
+kb_handler proc far ; new int 9h handler
+    pusha
+    push ES
+    push DS
+    push CS
+    add word ptr [0:041Ch], 3
+    pop DS
+    pop DS
+    pop ES
+    popa
+    jmp cs:old_9h
+    iret
 kb_handler endp
 main proc
     mov AX, @data
     mov DS, AX
-    mov ax, 3509h
+    xor CX, CX
+    
+   
+    mov AX, 3509h ; save old handler
     int 21h
-    mov word ptr old_9h, bx
-    mov word ptr old_9h[2], es
-    mov ax,2509h
-    mov dx ,offset kb_handler
+    mov word ptr old_9h, BX
+    mov word ptr old_9h+2, ES
+   
+    mov AX,2509h ; load new handler
+    mov DX, seg kb_handler 
+    mov DS, DX
+    mov DX, offset kb_handler
     int 21h   
-    mov ax, 3509h    
-    mov dx, offset old_9h
-    int 21h 
+   
+    back:
+    mov AH, 02h
+    mov DL, [0:041Ch]
+    int 21h
+    jmp back
+   
+    
+  
+    mov AH, 4Ch
+    int 21h
 main endp
-end main
+end main 
